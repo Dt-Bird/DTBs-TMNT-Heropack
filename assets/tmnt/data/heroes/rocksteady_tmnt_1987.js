c9af6278ad3121bb049d0d98be4bd137ed7fd21d
Dt-Bird <magregorich1@gmail.com>
2025-03-13 09:55:37 -0400

    rocksteady_tmnt_1987.js

---
+++ assets/tmnt/data/heroes/rocksteady_tmnt_1987.js
@@ -0,0 +1,12 @@
+function init(hero) {
+    hero.setName("Rocksteady");
+    hero.setVersion("1987");
+    hero.setTier(4);
+    
+    hero.setHelmet("item.superhero_armor.piece.mask");
+    hero.setChestplate("item.superhero_armor.piece.chestpiece");
+    hero.setLeggings("item.superhero_armor.piece.pants");
+    hero.setBoots("item.superhero_armor.piece.boots");
+	
+	hero.setDefaultScale(0.959459);
+}
