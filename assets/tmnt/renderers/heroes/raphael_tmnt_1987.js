9d3da5bcac7a470b448e750c4b78f6167b979941
Dt-Bird <magregorich1@gmail.com>
2025-03-13 10:43:21 -0400

    raphael_tmnt_1987.js

---
+++ assets/tmnt/renderers/heroes/raphael_tmnt_1987.js
@@ -0,0 +1,20 @@
+extend("fiskheroes:hero_basic");
+loadTextures({
+    "layer1": "tmnt:raphael_tmnt_1987_layer1",
+    "layer2": "tmnt:raphael_tmnt_1987_layer2"
+});
+
+function init(renderer) {
+    renderer.setTexture((entity, renderLayer) => renderLayer == "LEGGINGS" ? "layer2" : "layer1");
+
+    renderer.setItemIcons("%s_0", "%s_1", "%s_2", "%s_3");
+    renderer.showModel("HELMET", "head", "headwear");
+    renderer.showModel("CHESTPLATE", "body", "rightArm", "leftArm");
+    renderer.showModel("LEGGINGS", "body", "rightLeg", "leftLeg");
+    renderer.showModel("BOOTS", "rightLeg", "leftLeg");
+	renderer.fixHatLayer("HELMET");
+
+    initEffects(renderer);
+    initAnimations(renderer);
+}
+	
