df743c618d8348c01f48b222eaae6e90dda7b4f4
Dt-Bird <magregorich1@gmail.com>
2025-03-12 22:34:03 -0400

    Create april_oneil_1987.js

---
+++ assets/tmnt/data/heroes/april_oneil_1987.js
@@ -0,0 +1,12 @@
+function init(hero) {
+    hero.setName("April O'Neil");
+    hero.setVersion("1987");
+    hero.setTier(1);
+    
+    hero.setHelmet("item.superhero_armor.piece.mask");
+    hero.setChestplate("item.superhero_armor.piece.chestpiece");
+    hero.setLeggings("item.superhero_armor.piece.pants");
+    hero.setBoots("item.superhero_armor.piece.boots");
+	
+	hero.setDefaultScale(0.94);
+}
