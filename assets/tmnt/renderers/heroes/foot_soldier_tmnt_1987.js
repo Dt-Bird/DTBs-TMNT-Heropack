d2e783643bb9e671d620387d15649b3a2d066f93
Dt-Bird <magregorich1@gmail.com>
2025-03-14 21:50:10 -0400

    foot solider renderer
    properly separates the head and mask from the chestplate

--- assets/tmnt/renderers/heroes/foot_soldier_tmnt_1987.js
+++ assets/tmnt/renderers/heroes/foot_soldier_tmnt_1987.js
@@ -1,18 +1,20 @@
 extend("fiskheroes:hero_basic");
 loadTextures({
     "layer1": "tmnt:foot_soldier_tmnt_1987_layer1",
-    "layer2": "tmnt:foot_soldier_tmnt_1987_layer2"
+    "layer2": "tmnt:foot_soldier_tmnt_1987_layer2",
+	"mask": "tmnt:foot_soldier_tmnt_1987_mask"
 });
 
 function init(renderer) {
-    renderer.setTexture((entity, renderLayer) => renderLayer == "LEGGINGS" ? "layer2" : "layer1");
+    parent.init(renderer);
+    renderer.setTexture((entity, renderLayer) => {
+        if (entity.getWornHelmet().suitType() == $SUIT_NAME) {
+            return renderLayer == "HELMET" ? "mask" : renderLayer == "LEGGINGS" ? "layer2" : "layer1";
+        }
+        return renderLayer == "LEGGINGS" ? "layer2" : "layer1";
+    });
 
-    renderer.setItemIcons("%s_0", "%s_1", "%s_2", "%s_3");
-    renderer.showModel("HELMET", "head", "headwear");
-    renderer.showModel("CHESTPLATE", "body", "rightArm", "leftArm");
-    renderer.showModel("LEGGINGS", "body", "rightLeg", "leftLeg");
-    renderer.showModel("BOOTS", "rightLeg", "leftLeg");
-	renderer.fixHatLayer("HELMET");
+    renderer.showModel("HELMET", "head", "headwear", "body", "rightArm", "leftArm");
 
     initEffects(renderer);
     initAnimations(renderer);
