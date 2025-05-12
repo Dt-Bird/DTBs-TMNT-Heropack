f21ef9cff82b8e3ddf6750eeac5bb4579340a020
Dt-Bird <magregorich1@gmail.com>
2025-05-11 21:02:29 -0400

    street alt renderers

---
+++ assets/tmnt/renderers/heroes/donatello_tmnt_1987_street.js
@@ -0,0 +1,18 @@
+extend("tmnt:donatello_tmnt_1987");
+loadTextures({
+    "layer1": "tmnt:donatello_tmnt_1987_street_layer1",
+    "layer2": "tmnt:donatello_tmnt_1987_street_layer2"
+});
+
+var utils = implement("fiskheroes:external/utils");
+
+function init(renderer) {
+    parent.init(renderer);
+    renderer.setTexture((entity, renderLayer) => renderLayer == "LEGGINGS" ? "layer2" : "layer1");
+
+    renderer.showModel("HELMET", "head", "headwear");
+    renderer.showModel("CHESTPLATE", "body", "rightArm", "leftArm");
+    renderer.showModel("LEGGINGS", "body", "rightLeg", "leftLeg");
+    renderer.showModel("BOOTS", "rightLeg", "leftLeg");
+    renderer.fixHatLayer("HELMET");
+}
